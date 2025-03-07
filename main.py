96a785eac6b6c99623755dbba22ddd3bea29b8a7
Kalinote <knote840746219@gmail.com>
2025-03-07 15:12:57 +0800

    update

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from astrbot.api import logger
 class KNBotBase(Star):
     def __init__(self, context: Context):
         super().__init__(context)
+        logger.info("KNBot Base 初始化")
+
+
+        logger.info("所有插件: {}".format(self.context.get_all_stars()))
 
         # 禁用原生提醒插件
+        logger.info("禁用原生提醒插件")
         self.context._star_manager.turn_off_plugin("astrbot-reminder")
         
 
