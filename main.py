10d400c1a82e1b74addf4c5766857bdfa6720b0a
Kalinote <knote840746219@gmail.com>
2025-03-07 16:37:28 +0800

    develop update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
@@ -7,14 +8,18 @@ from astrbot.api import logger
 class KNBotBase(Star):    
     def __init__(self, context: Context):
         super().__init__(context)
+        
+        self.scheduler = AsyncIOScheduler(timezone="Asia/Shanghai")
 
     @filter.on_astrbot_loaded()
     async def on_astrbot_loaded(self):
         for plugin in self.context.get_all_stars():
-            if plugin.name == "astrbot-reminder":
+            if plugin.name == "astrbot-reminder" and plugin.activated:
                 logger.info("禁用原生提醒插件")
                 await self.context._star_manager.turn_off_plugin("astrbot-reminder")
-            if plugin.name == "astrbot-python-interpreter":
+            if plugin.name == "astrbot-python-interpreter" and plugin.activated:
                 logger.info("禁用原生代码执行器插件")
                 await self.context._star_manager.turn_off_plugin("astrbot-python-interpreter")
+    
+    
     
