acfe3ab235f70b6cd4d32c605926505aed99babd
Kalinote <knote840746219@gmail.com>
2025-03-07 12:52:09 +0800

    update

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
 
-@register("KNBot Base", "Kalinote", "KNBot 基础功能插件", "1.0.0", "https://github.com/kalinote/astrbot_plugin_knbot")
+@register("KNBot Base", "Kalinote", "KNBot 基础功能插件", "0.1", "https://github.com/kalinote/astrbot_plugin_knbot")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
