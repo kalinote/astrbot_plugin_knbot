0d0d2c1150bef524e9ca662daa0f9975228dc7b8
Kalinote <knote840746219@gmail.com>
2025-03-07 16:03:39 +0800

    初始化时禁用功能冲突的原生插件

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from astrbot.api import logger
 
 
 @register("KNBot Base", "Kalinote", "KNBot 基础功能插件", "0.1", "https://github.com/kalinote/astrbot_plugin_knbot")
-class KNBotBase(Star):
+class KNBotBase(Star):    
     def __init__(self, context: Context):
         super().__init__(context)
-        logger.info("KNBot Base 初始化")
 
-
-        logger.info("所有插件: {}".format(self.context.get_all_stars()))
-
-        # 禁用原生提醒插件
-        logger.info("禁用原生提醒插件")
-        self.context._star_manager.turn_off_plugin("astrbot-reminder")
-        
-
+    @filter.on_astrbot_loaded()
+    async def on_astrbot_loaded(self):
+        for plugin in self.context.get_all_stars():
+            if plugin.name == "astrbot-reminder":
+                logger.info("禁用原生提醒插件")
+                await self.context._star_manager.turn_off_plugin("astrbot-reminder")
+            if plugin.name == "astrbot-python-interpreter":
+                logger.info("禁用原生代码执行器插件")
+                await self.context._star_manager.turn_off_plugin("astrbot-python-interpreter")
+    
